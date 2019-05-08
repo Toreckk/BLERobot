be0f08eb4dc8d6d97177c0c7f9237cc5f90f8b76
Juan Coret <coret.juan@gmail.com>
2019-05-08 22:06:18 +0200

    BLE Scan created
    Ble Scan created
    Control Activity Started

--- Android App/app/src/main/java/com/bletest/blerobotapp/ControlActivity.java
+++ Android App/app/src/main/java/com/bletest/blerobotapp/ControlActivity.java
@@ -2,12 +2,122 @@ package com.bletest.blerobotapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 
 public class ControlActivity extends AppCompatActivity {
+
+    private SeekBar seekbar;
+    private SeekBar seekbar2;
+    private TextView textView;
+    private TextView textView2;
+
+    private Button button;
+
+    // This tag is used for debug messages
+    private static final String TAG = ControlActivity.class.getSimpleName();
+
+    private static String mDeviceAddress;
+    private static RobotService mRobotService;
+
+
+    /**
+     * This manages the lifecycle of the BLE service.
+     * When the service starts we get the service object, initialize the service, and connect.
+     */
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            Log.i(TAG, "onServiceConnected");
+            mRobotService = ((RobotService.LocalBinder) service).getService();
+            if (!mRobotService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the car database upon successful start-up initialization.
+            mRobotService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mRobotService = null;
+        }
+    };
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName)
+    {
+        
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+        seekbar2 = (SeekBar) findViewById(R.id.seekBar2);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int newprogress = progress-100;
+                textView.setText("" + newprogress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekbar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int newprogress = progress-100;
+                textView2.setText("" + newprogress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        button = (Button) findViewById(R.id.ButtonStop);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekbar.setProgress(100);
+                seekbar2.setProgress(100);
+            }
+        });
     }
 }
